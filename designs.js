80951b5107a5fac70d03ed0da25483f7a6408985
Pedro Paulo B. Grein <pedro.grein@gmail.com>
2018-11-08 18:03:18 -0200

    Add the grid logic

--- designs.js
+++ designs.js
@@ -1,12 +1,33 @@
 // Select color input
+
 // Select size input
+var sizeX = $('#inputWidth').val();
+var sizeY = $('#inputHeight').val();
+
+$('#inputWidth').change(function(){
+	sizeX = $(this).val();
+	console.log(sizeX);
+});
+$('#inputHeight').change(function(){
+	sizeY = $(this).val();
+	console.log(sizeY);
+});
 
 // When size is submitted by the user, call makeGrid()
+$('input[type=submit]').click(function(event){
+	event.preventDefault();
+	$('#pixelCanvas tr').remove();
+	makeGrid();
+});
 
+//construct the Grid based on the data submitted
 function makeGrid() {
-
-	const column = $('#inputHeight').val();
-	console.log('column');
-// Your code goes here!
-
+	for (y = 0; y < sizeY; y++ ){
+		$('#pixelCanvas').append('<tr>');
+			for(x = 0; x< sizeX; x++ ){
+				$('#pixelCanvas tr:last-child').append('<td></td>');
+			}
+		$('#pixelCanvas').append('</tr>');
+	}
+	console.log(sizeX + sizeY);// Your code goes here!
 }
